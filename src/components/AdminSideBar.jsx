a2f72cac76bee23a79af1065afad3e6cdf23b7e3
Gunjankher <gkhercode@gmail.com>
2024-06-18 16:25:20 +0530

    sidebar

--- src/components/AdminSideBar.jsx
+++ src/components/AdminSideBar.jsx
@@ -1,17 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import { RiDashboardFill } from 'react-icons/ri';
 
 function AdminSideBar() {
-  return (
-   <aside>
-    <h2>Logo.</h2>
-    <div>
-        <h5>Dashboard</h5>
-    </div>
+    const location = useLocation();
 
+    const dashItems = [
+        {
+            icon: <RiDashboardFill />,
+            name: "Dashboard",
+            path: "/admin/dashboard",
+        },
+        {
+            icon: <RiDashboardFill />,
+            name: "Products",
+            path: "/admin/products",
+        },
+        {
+            icon: <RiDashboardFill />,
+            name: "Customer",
+            path: "/admin/customers",
+        },
+        {
+            icon: <RiDashboardFill />,
+            name: "Transaction",
+            path: "/admin/transaction",
+        },
+    ];
 
-
-   </aside>
-  )
+    return (
+        <aside>
+            <h2>Logo</h2>
+            <div>
+                <h5>Dashboard</h5>
+                <ul>
+                    {dashItems.map((item, index) => (
+                        <li
+                            key={index}
+                            className={location.pathname === item.path ? 'active' : ''}
+                        >
+                            <Link to={item.path}>
+                                {item.icon}
+                                {item.name}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </aside>
+    );
 }
 
-export default AdminSideBar
+export default AdminSideBar;
