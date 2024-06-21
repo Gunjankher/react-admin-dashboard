0fb498380b183957f38739908b0f0aa2d82c85a7
Gunjankher <gkhercode@gmail.com>
2024-06-21 15:25:27 +0530

    chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -7,8 +7,9 @@ import {
   Title,
   Tooltip,
   Legend,
+  ArcElement
 } from 'chart.js';
-import { Bar } from 'react-chartjs-2';
+import { Bar,Doughnut } from 'react-chartjs-2';
 
 ChartJS.register(
   CategoryScale,
@@ -16,7 +17,8 @@ ChartJS.register(
   BarElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  ArcElement
 );
 
 
@@ -30,7 +32,7 @@ data_2 = [],
 title_1,
 title_2,
 bgColor_1,
-bgColor_2,
+bgColor_2, 
 horizontal = false,
 lables = "months"
 
@@ -42,13 +44,34 @@ lables = "months"
          
         plugins: {
           legend: {
-            position: 'top'
+            position: 'top',
+            display : false,
+           
+            
           },
           title: {
             display: false,
             text: 'Chart.js Bar Chart',
+
           },
         },
+
+        scales :{
+            y:{
+                beginAtZero : true,
+                grid:{
+                    display : false
+                },
+            },
+            x:{
+              
+                grid:{
+                    display : false
+                },
+            },
+        }
+
+
       };
       
       
@@ -58,13 +81,19 @@ lables = "months"
           {
             label: title_1,
             data: data_1,
-            backgroundColor : bgColor_1
+            backgroundColor : bgColor_1,
+            barThickness:"flex",
+            barPercentage : 1,
+            categoryPercentage:0.4,
             
           },
           {
             label: title_2,
             data: data_2,
-                backgroundColor :bgColor_2
+                backgroundColor :bgColor_2,
+                barThickness:"flex",
+                barPercentage : 1,
+                categoryPercentage:0.4,
           },
         ],
       };
@@ -73,3 +102,49 @@ lables = "months"
 
   return <Bar options={options} data={data} />;
 }
+
+
+
+// import { ChartData, ChartOptions } from 'chart.js'; 
+
+// Define the DoughnutChart component
+export const DoughnutChart = ({
+  labels,
+  data,
+  backgroundColor,
+  cutout,
+  legends = true,
+  offset,
+}) => {
+  // Define the data for the doughnut chart
+  const doughnutData = {
+    labels,
+    datasets: [
+      {
+        data,
+        backgroundColor,
+        borderWidth: 0,
+        offset,
+      },
+    ],
+  };
+
+  // Define the options for the doughnut chart
+  const doughnutOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: legends,
+        position: 'bottom',
+        labels: {
+          padding: 40,
+        },
+      },
+    },
+    cutout,
+  };
+
+  // Render the Doughnut chart
+  return <Doughnut data={doughnutData} options={doughnutOptions} />;
+};
+
--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -6,109 +6,143 @@ import Input from "../components/Input"
 import Widget from '../components/Widget'
 import CategoryItem from '../components/CategoryItem'
 import data from '../assets/data.json'
-import {BarChart} from '../components/Chart'
+import { BarChart,DoughnutChart } from '../components/Chart'
 
 import userImg from "../assets/profile.jpg"
-import {FaRegBell} from "react-icons/fa"
-import {BsSearch} from "react-icons/bs"
+import { FaRegBell } from "react-icons/fa"
+import { BsSearch } from "react-icons/bs"
+import { BiMaleFemale } from "react-icons/bi"
 
 function DashBoard() {
-const userImg = "img.freepik.com/premium-vector/anonymous-user-circle-icon-vector-illustration-flat-style-with-long-shadow_520826-1931.jpg"
+  const userImg = "img.freepik.com/premium-vector/anonymous-user-circle-icon-vector-illustration-flat-style-with-long-shadow_520826-1931.jpg"
 
   return (
     <div className='adminContainer'>
-<AdminSideBar/>
-<main className='dashboard'>
-<div className='bar'>
-<BsSearch />
-<Input
-type ="text"
-placeholder = "Search for data,users,docs"
-/>
-<img src={userImg} alt="" />
-<FaRegBell />
-
-</div>
-
-<section className='WidgetContainer'>
-
-<Widget  
- heading="Revenue"
- value={1000000}
- percent={30}
- color="blue"
- amount={500}
-
-/>
-<Widget  
- heading="Revenue"
- value={1000}
- percent={-30}
- color="aquamarine"
- amount={false}
-
-/>
-<Widget  
- heading="Revenue"
- value={1000}
- percent={30}
- color="orange"
-//  amount={500}
-
-/>
-<Widget  
- heading="Revenue"
- value={100000} 
- percent={30}
- color="purple"
-//  amount={500}
-
-/>
+      <AdminSideBar />
+      <main className='dashboard'>
 
 
-</section>
+        <div className='bar'>
+          <BsSearch />
+          <Input
+            type="text"
+            placeholder="Search for data,users,docs"
+          />
+          <img src={userImg} alt="" />
+          <FaRegBell />
 
-<section className='graph-container'>
-<div className="revenue-chart">
-<h2>Revenue & Transaction</h2>
-<BarChart
-data_1={[300,144,433,655,237,755,190 ]}
-data_2={[200,444,553,675,127,345,490 ]}
-title_1='revenue'
-title_2='Transaction'
-bgColor_1='rgb(0,115,255)'
-bgColor_2='rgba(53,162,235,0.8)'
+        </div>
 
-/>
+
+        <section className='WidgetContainer'>
+
+          <Widget
+            heading="Revenue"
+            value={1000000}
+            percent={30}
+            color="blue"
+            amount={500}
+
+          />
+          <Widget
+            heading="Revenue"
+            value={1000}
+            percent={-30}
+            color="aquamarine"
+            amount={false}
+
+          />
+          <Widget
+            heading="Revenue"
+            value={1000}
+            percent={30}
+            color="orange"
+          //  amount={500}
+
+          />
+          <Widget
+            heading="Revenue"
+            value={100000}
+            percent={30}
+            color="purple"
+          //  amount={500}
+
+          />
+
+
+        </section>
 
 
 
-</div>
+        <section className='graph-container'>
+          <div className="revenue-chart">
+            <h2>Revenue & Transaction</h2>
+            <BarChart
+              data_1={[300, 144, 433, 655, 237, 755, 190]}
+              data_2={[200, 444, 553, 675, 127, 345, 490]}
+              title_1='revenue'
+              title_2='Transaction'
+              bgColor_1='rgb(0,115,255)'
+              bgColor_2='rgba(53,162,235,0.8)'
+
+            />
+
+
+
+          </div>
 
 
 
 
 
-<div className="dashboard-categories">
-  <h2>Inventory</h2>
-  <div>
-   {data.categories.map((i)=>(
-     <CategoryItem 
-     key={i.heading}
-     heading={i.heading}
-      value={i.value}
-       color= {`hsl(${i.value*4},${i.value}%,50%)`} 
-       />
-   ))}
-  </div>
-</div>
+          <div className="dashboard-categories">
+            <h2>Inventory</h2>
+            <div>
+              {data.categories.map((i) => (
+                <CategoryItem
+                  key={i.heading}
+                  heading={i.heading}
+                  value={i.value}
+                  color={`hsl(${i.value * 4},${i.value}%,50%)`}
+                />
+              ))}
+            </div>
+          </div>
 
 
 
-</section>
+        </section>
 
 
-</main>
+
+        <section className='transation-container'>
+
+          <div className="gender-chart">
+            <h2>Gender Ratio</h2>
+            {/* chart */}
+            <DoughnutChart 
+            labels={['female,male']}
+            data={[12,19]}
+            backgroundColor={['hsl(340,82%,56%)','rgba(53,162,235,0.8)']}
+            cutout={90}
+            />
+<p>
+  <BiMaleFemale/>
+   </p>
+          </div>
+
+          {/* Table */}
+
+
+
+
+
+
+        </section>
+
+
+
+      </main>
 
     </div>
   )
